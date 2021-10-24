1133e070bc8e5611c8f06b012bcddc2af90330da
0x5ubt13 <5ubt13@protonmail.com>
2021-10-24 11:36:06 +0100

    while loop example

--- src/Main.java
+++ src/Main.java
@@ -58,6 +58,21 @@ public class Main {
         } else {
             System.out.println("Your shoe selection will make you very happy today");
         }
+
+        // Loops
+        Scanner whileLoop = new Scanner(System.in);
+        boolean isOnRepeat = true;
+        while(isOnRepeat) {
+            System.out.println("Playing current song");
+            System.out.println("Would you like to take this song off of repeat? If so, answer yes");
+            String userInput = whileLoop.next();
+
+            if(userInput.equals("yes")){
+                isOnRepeat = false;
+            }
+        }
+        System.out.println("Playing next song");
+
         
     }
 }
